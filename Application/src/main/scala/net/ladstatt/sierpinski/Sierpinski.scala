7404154b32972b1eb376e7c2f8fc9f5791abd11e
ielnykov <viktor.ielnykov@edu.fh-joanneum.at>
2016-12-16 00:07:38 +0100

    changes

--- Application/src/main/scala/net/ladstatt/sierpinski/Sierpinski.scala
+++ Application/src/main/scala/net/ladstatt/sierpinski/Sierpinski.scala
@@ -1,6 +1,24 @@
 package net.ladstatt.sierpinski
 
-// TODO
-// implement here all domain logic
-// Pos / Triangle / Sierpinski
+import android.graphics.Path.FillType
+import android.graphics.{Canvas, Paint, Path}
+
+case class Pos(x: Double, y: Double)
+
+case class Triangle(a: Pos, b: Pos, c: Pos, d: Int, canvas: Canvas, paint: Paint) {
+  if (d > 0) {
+    Triangle(a, Pos((a.x + c.x) / 2.0, (a.y + c.y) / 2.0), Pos((a.x + b.x) / 2.0, (a.y + b.y) / 2.0), d - 1, canvas, paint)
+    Triangle(Pos((a.x + b.x) / 2.0, (a.y + b.y) / 2.0), Pos((b.x + c.x) / 2.0, (b.y + c.y) / 2.0), b, d - 1, canvas, paint)
+    Triangle(Pos((a.x + c.x) / 2.0, (a.y + c.y) / 2.0), Pos((b.x + c.x) / 2.0, (b.y + c.y) / 2.0), c, d - 1, canvas, paint)
+  } else {
+    val path = new Path
+    path.setFillType(FillType.EVEN_ODD)
+    path.moveTo(a.x.toFloat, a.y.toFloat)
+    path.lineTo(b.x.toFloat, b.y.toFloat)
+    path.lineTo(c.x.toFloat, c.y.toFloat)
+    path.lineTo(a.x.toFloat, a.y.toFloat)
+    path.close
+    canvas.drawPath(path, paint)
+  }
+}
 
--- Application/src/main/scala/net/ladstatt/sierpinski/SierpinskiView.scala
+++ Application/src/main/scala/net/ladstatt/sierpinski/SierpinskiView.scala
@@ -1,22 +1,34 @@
-
 package net.ladstatt.sierpinski
 
 import android.content.Context
 import android.graphics._
-import android.util.{AttributeSet, SparseArray}
-import android.view.{MotionEvent, View}
+import android.util.AttributeSet
+import android.view.View
 
-import scala.Option
-
-/**
-  * Implements a view which displays a sierpinski triangle.
-  *
-  * @param context
-  * @param attrs
-  */
 class SierpinskiView(val context: Context, val attrs: AttributeSet) extends View(context, attrs) {
 
+  override protected def onDraw(canvas: Canvas): Unit = {
+    super.onDraw(canvas)
 
+    val paint = new Paint
+    val w = getWidth
+    val h = getHeight
 
+    val tw = w - 40
+    val th = Math.sqrt(tw * tw - (tw / 2) * (tw / 2))
+    val tt = h / 2 - th / 2
+    val tb = tt + th
+
+    val a = Pos(20, tb)
+    val b = Pos(w - 20, tb)
+    val c = Pos(tw / 2, tt)
+
+    paint.setAntiAlias(true)
+    paint.setColor(android.graphics.Color.BLACK)
+    paint.setStyle(Paint.Style.STROKE)
+    paint.setStrokeWidth(2)
+
+    Triangle(a, b, c, 4, canvas, paint)
+  }
 
 }
